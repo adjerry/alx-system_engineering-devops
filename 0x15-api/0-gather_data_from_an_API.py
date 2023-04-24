5f77655c4870d75654b1dda5573c7a5926d03168
root <adjerusa@gmail.com>
2023-04-24 08:34:23 -0700

    task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,13 +9,13 @@ from sys import argv
 if __name__ == "__main__":
     userID = argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-            format(userID)).json()
+                        format(userID)).json()
     todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-            .format(userID)).json()
+                         .format(userID)).json()
     completed_tasks = []
     for task in todos:
         if task.get('completed') is True:
             completed_tasks.append(task.get('title'))
     print("Employee {} is done with tasks({}/{}):".
-            format(user.get('name'), len(completed_tasks), len(todos)))
+          format(user.get('name'), len(completed_tasks), len(todos)))
     print("\n".join("\t {}".format(task) for task in completed_tasks))
